55c8a405414876fee211a64f1e1c8426fd4071fd
Oscar Rojas <rojasoscar752@gmail.com>
2025-09-10 11:30:59 -0500

    fix(#37): aplicar camelCase y limpiar app.js

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
 require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var listaQuejasRouter = require('./routes/listaQuejas');
-var quejasRouter = require('./routes/quejas');
-var reportesRouter = require('./routes/reportes');
+// Rutas
+const listaQuejasRouter = require('./routes/listaQuejas');
+const quejasRouter = require('./routes/quejas');
+const reportesRouter = require('./routes/reportes');
 const { loadEntidades } = require('./services/quejas.service');
 
-var app = express();
+const app = express();
 
-
+// Configuración de vistas
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
-
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'src/public')));
 
-app.use(express.static(path.join(__dirname, 'src/views')));
-
-
+// Rutas
 app.use('/api/quejas', quejasRouter);
 app.use('/api/reportes', reportesRouter);
 app.use('/', listaQuejasRouter);
 
+// Inicializar entidades
 loadEntidades();
 
-
-app.use(function (req, res, next) {
+// catch 404
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-
-app.use(function (err, req, res) {
+// Error handler
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   const isApi = req.originalUrl && req.originalUrl.startsWith('/api');
   const wantsJson = req.accepts && req.accepts('json') === 'json';
@@ -51,13 +52,11 @@ app.use(function (err, req, res) {
     });
   }
 
-  
   res.render(
     'error',
     { message: err.message, error: req.app.get('env') === 'development' ? err : {} },
-    function (renderErr, html) {
+    (renderErr, html) => {
       if (renderErr) {
-        
         console.error('No se pudo renderizar la vista de error:', renderErr.message);
         return res.send(`<h1>Error</h1><pre>${err.message}</pre>`);
       }
