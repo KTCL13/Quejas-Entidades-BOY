a6e653947c47b3a921688183b83052865fc1b68b
KTCL13 <cesard20101@gmail.com>
2025-09-21 22:02:58 -0500

    se crea el servicio y la ruta para obtener los comentarios de una queja

--- routes/comments.js
+++ routes/comments.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
+const { getCommentsByComplaintId } = require('../services/comment.service');
 
 //Get /api/comments-complaint-id/:id
-router.get('/:id', async (req, res) => {
+router.get('/complaint-id/:id', async (req, res) => {
   const complaintId = req.params.id;
   if (!isNaN(complaintId)) {
-    
-    res.send(`Comentarios para la queja con ID: ${complaintId}`);
+    var comments = await getCommentsByComplaintId(complaintId);
+    res.json(comments);
   } else {
     res.status(400).send('ID de queja inválido');
   }
 });
+
+
 
 module.exports = router;
