87cc3f3fc8114d01809d1a909f5de6b502afebd0
KTCL13 <cesard20101@gmail.com>
2025-09-21 19:18:09 -0500

    Se crea un endpoint para obtener una queja por su ID

--- services/quejas.service.js
+++ services/quejas.service.js
@@ -134,3 +134,19 @@ exports.changeComplaintState = async (complaintId, newState) => {
     throw new Error('Error al cambiar el estado de la queja');
   }
 };
+
+exports.getComplaintById = async (complaintId) => {
+  try {
+    const complaint = await Complaint.findOne({
+      where: { id: complaintId, is_deleted: false },
+      include: [{
+        model: Entity, 
+        attributes: ['id', 'name']
+      }],
+    });
+    return complaint;
+  } catch (error) {
+    console.error('Error al obtener la queja por ID:', error);
+    throw new Error('Error al obtener la queja por ID');
+  }
+};
