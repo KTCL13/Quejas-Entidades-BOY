6dfe17b28c7f424ab9737d7f3cbbecf8a4719b35
KTCL13 <cesard20101@gmail.com>
2025-09-23 12:18:32 -0500

    Se actualizan los seeders de entidades para que correspondan a entidades públicas de Boyacá.

--- seeders/20250908160117-demo-entidades.js
+++ seeders/20250908160117-demo-entidades.js
@@ -4,10 +4,11 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('entities', [
-      { name: 'Acueducto y Alcantarillado de Tunja' },
-      { name: 'Veolia - Aseo' },
-      { name: 'Empresa de Energía de Boyacá' },
-      { name: 'Servicio de Gas Natural' }
+      { name: 'Secretaría De Educación De Boyacá' },
+      { name: 'Secretaría de Ambiente y Desarrollo Sostenible' },
+      { name: 'Sistema de Atención al Ciudadano' },
+      { name: 'Secretaría de Planeación' },
+      { name: 'Secretaría de TIC y Gobierno Abierto' },
     ], {});
   },
 
