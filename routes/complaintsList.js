c0463453ff144442e08e76da1a64c3aa60d61fe1
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-09-24 15:50:48 -0500

    Se arreglan los endpoints y formato de dato en la pagina de especial de cada queja (#157)
    * Se arreglan los endpoints y formato de datos para que se mostrara todo correctamente en el frontend
    
    * se restablece el post endopoint a como estaba antes
    
    * Se arreglan los endpoints y formato de datos para que se mostrara todo correctamente en el frontend
    
    * se restablece el post endopoint a como estaba antes
    
    * Se arreglan los endpoints y formato de datos para que se mostrara todo correctamente en el frontend
    
    * se restablece el post endopoint a como estaba antes

--- routes/complaintsList.js
+++ routes/complaintsList.js
@@ -15,22 +15,37 @@ router.get('/', async (req, res) => {
   });
 });
 
+
 router.get('/complaint/:id', async (req, res) => {
   const { id } = req.params;
 
-  const complaint = {
-    id,
-    entities: "Entidad X",
-    state: "Pendiente",
-    description: "Texto de la queja...",
-    comments: [
-    ]
-  };
+  try {
 
-  res.render('comment', {
-    complaint,
-    activePage: 'detalle'
-  });
+    const apiResponse = await fetch(`http://localhost:3000/api/complaints/${id}`);
+
+
+    if (!apiResponse.ok) {
+      return res.status(apiResponse.status).send('Queja no encontrada');
+    }
+
+    const data = await apiResponse.json();
+
+    const complaint = {
+      id: data.id,
+      entities: data.Entity.name,
+      state: data.state,
+      description: data.description,
+    };
+
+    res.render('comment', {
+      complaint,
+      activePage: 'detalle'
+    });
+
+  } catch (error) {
+    console.error('Error al obtener los datos de la queja:', error);
+    res.status(500).send('Error interno del servidor');
+  }
 });
 
 module.exports = router;
