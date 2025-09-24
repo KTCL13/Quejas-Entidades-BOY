2702ce9121d79901a552c34da3504f491161718c
KTCL13 <cesard20101@gmail.com>
2025-09-24 17:31:14 -0500

    se cambia sendgrind en el servicio de correo de reportes

--- routes/reportes.js
+++ routes/reportes.js
@@ -1,45 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer = require("nodemailer");
+const mailService = require('../services/sendgrid.service');
 require("dotenv").config(); // <--- Importar dotenv
 
-// Configuración del transporter con variables de entorno
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
 
 // GET /api/reports
 router.get('/', async (req, res) => {
   res.render('reportes', { activePage: 'reportes', mensaje: null });
+
 });
+
 
 // GET /api/reports/ver
 router.get('/ver', async (req, res) => {
-  const clientIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_TO, // ahora configurable
-    subject: "Notificación: Ver Reporte",
-    text: `Se hizo clic en "Número de quejas por entidad" desde la IP: ${clientIp}`,
-  };
-
+  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   try {
-    await transporter.sendMail(mailOptions);
-    res.render('reportes', { 
-      activePage: 'reportes',
-      mensaje: "Se envió el correo correctamente"
+    await mailService.sendMail({
+      to: process.env.EMAIL_TO,
+      subject: 'Reporte de problema en el sistema de quejas',
+      text: `Se ha recibido un reporte de problema desde la IP: ${clientIp}. Por favor, revisar el sistema para más detalles.`,
     });
   } catch (error) {
-    console.error("Error al enviar correo:", error);
-    res.render('reportes', { 
-      activePage: 'reportes',
-      mensaje: " Error al enviar correo"
-    });
+    console.error('Error al enviar el correo:', error);
   }
 });
 
