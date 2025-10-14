fb03738fd80f2aa59f62419bed7f84d2ff1dfb10
KTCL13 <cesard20101@gmail.com>
2025-10-14 18:38:56 -0500

    Se agrega cambia la configuración de ESlint para permitir console.warn y console.error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,30 @@
+// eslint.config.mjs
 import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
 import globals from "globals";
+import pluginJest from "eslint-plugin-jest";
+import prettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
-import jest from "eslint-plugin-jest";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        "bootstrap": "readonly",
-        "grecaptcha": "readonly"
-      }
-    }
+export default defineConfig({
+  files: ["**/*.{js,mjs,cjs}"],
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.browser,
+      ...globals.jest,
+      bootstrap: "readonly",
+      grecaptcha: "readonly",
+    },
   },
-  {
-    files: ["**/*.js"], languageOptions:
-      { sourceType: "commonjs" }
+  plugins: {
+    "@stylistic": stylistic,
+    jest: pluginJest,
   },
-  {
-    files: ["test-jest/**/*.test.js"],
-    ...jest.configs["flat/recommended"],
-    rules: {
-      ...jest.configs["flat/recommended"].rules
-    }
+  extends: [js.configs.recommended, prettier],
+  rules: {
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "no-undef": "error",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
   },
-  {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "off",
-      "no-console": "warn",
-    }
-  }
-]);
+});
