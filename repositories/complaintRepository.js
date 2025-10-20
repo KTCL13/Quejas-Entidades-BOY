1eee935b0b9638c6460e638d0945d6f2abab5a0b
Haroldmedina2003 <haromedina2003@gmail.com>
2025-10-20 09:36:36 -0500

    aplicar formato Prettier en complaintRepository.js

--- repositories/complaintRepository.js
+++ repositories/complaintRepository.js
@@ -49,9 +49,8 @@ exports.createComplaint = async ({ description, entity_id }) => {
   }
 };
 
-
 module.exports = {
   getComplaintById,
   updateComplaintState,
-  createComplaint
+  createComplaint,
 };
