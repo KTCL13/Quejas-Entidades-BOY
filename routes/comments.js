216aef9e8304672fc7d4a9750bc1d44a61557a5a
Cesar Pérez Fonseca <cesard20101@gmail.com>
2025-10-20 18:31:10 -0500

    Se separa el endpoint para crear comentarios en un ruta y controllador y se actualizan las pruebas de integración

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createCommentByComplaintId } = require('../services/comment.service');
-const { param } = require('express-validator');
+const { param, body } = require('express-validator');
 const { validateRequest } = require('../middleware/validateRequest');
 const {
   getCommentsByComplaintIdController,
+  createCommentByComplaintIdController,
 } = require('../controllers/commentsController');
 
 //Get /api/complaint/:id
@@ -15,15 +15,12 @@ router.get(
   getCommentsByComplaintIdController
 );
 
-router.post('/complaint/:id', async (req, res) => {
-  const complaintId = req.params.id;
-  const { message } = req.body;
-  if (!isNaN(complaintId) && message) {
-    const newComment = await createCommentByComplaintId(complaintId, message);
-    res.status(201).json(newComment);
-  } else {
-    res.status(400).send('ID de queja inválido o contenido vacío');
-  }
-});
+router.post(
+  '/complaint/:id',
+  param('id').isNumeric().withMessage('ID de queja inválido'),
+  body('message').notEmpty().withMessage('El mensaje no puede estar vacío'),
+  validateRequest,
+  createCommentByComplaintIdController
+);
 
 module.exports = router;
