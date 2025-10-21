754372d7a07fd81fe02612a238658135dc11d804
Cesar Pérez Fonseca <cesard20101@gmail.com>
2025-10-20 22:49:24 -0500

    config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: 'node',
+  globalSetup: '<rootDir>/test-jest/global-setup.js',
+  globalTeardown: '<rootDir>/test-jest/global-teardown.js',
+};
