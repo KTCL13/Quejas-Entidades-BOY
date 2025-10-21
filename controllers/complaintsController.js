8392b770583346d7a2879c420c857757b25863d8
Cesar Pérez Fonseca <cesard20101@gmail.com>
2025-10-20 23:25:33 -0500

    Se agrega funcion createComplaint controller en la capa de controlador de quejas para gestionar  la creación de quejas

--- controllers/complaintsController.js
+++ controllers/complaintsController.js
@@ -1,10 +1,23 @@
-const { getComplaintById } = require('../services/quejas.service');
+const {
+  getComplaintById,
+  createComplaint,
+} = require('../services/quejas.service');
 
 exports.getComplaintByIdController = async (req, res, next) => {
   try {
     const { id } = req.params;
     const complaint = await getComplaintById(id);
     return res.status(200).json(complaint);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createComplaintController = async (req, res, next) => {
+  try {
+    const complaintData = req.body;
+    const newComplaint = await createComplaint(complaintData);
+    return res.status(201).json(newComplaint);
   } catch (error) {
     next(error);
   }
