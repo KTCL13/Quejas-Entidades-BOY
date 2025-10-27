6ae549510194248e450e348282c35a025e080d4d
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-27 14:57:51 -0500

    Se agrega validación de sesion a la ruta de obtener la lista páginada de quejas. (#229)

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,6 +1,9 @@
-import { validationResult } from 'express-validator';
+const { validationResult } = require('express-validator');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const authServiceUrl = process.env.AUTH_SERVICE_URL;
 
-export const validateRequest = (req, res, next) => {
+exports.validateRequest = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({
@@ -11,4 +14,29 @@ export const validateRequest = (req, res, next) => {
     });
   }
   next();
+};
+
+exports.validateLogin = async (req, res, next) => {
+  try {
+    const userEmail = req.headers['x-useremail'];
+
+    const response = await axios.post(`${authServiceUrl}/session-status`, {
+      email: userEmail,
+    });
+
+    const { sessionActive } = response.data;
+
+    if (!sessionActive) {
+      return res.status(401).json({
+        message: 'Usuario con sesion inactiva',
+      });
+    }
+    next();
+  } catch (error) {
+    console.error('Error al validar sesión:', error.message);
+    res.status(500).json({
+      success: false,
+      message: 'Error al validar el estado de sesión.',
+    });
+  }
 };
