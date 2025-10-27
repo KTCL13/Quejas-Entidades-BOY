3dafc31000ca02f59fbbb97bc0a752f31e8086ea
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-27 12:18:01 -0500

    Se agrega la función getComplaintListController para manejar la obtención de quejas por entidad con paginación. (#226)

--- controllers/complaintsController.js
+++ controllers/complaintsController.js
@@ -1,6 +1,7 @@
 const {
   getComplaintById,
   createComplaint,
+  getComplaintListByEntity,
 } = require('../services/quejas.service');
 
 exports.getComplaintByIdController = async (req, res, next) => {
@@ -18,6 +19,19 @@ exports.createComplaintController = async (req, res, next) => {
     const complaintData = req.body;
     const newComplaint = await createComplaint(complaintData);
     return res.status(201).json(newComplaint);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getComplaintListController = async (req, res, next) => {
+  try {
+    const entityId = parseInt(req.query.entidadId, 10);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const complaints = await getComplaintListByEntity(entityId, page, limit);
+    return res.status(200).json(complaints);
   } catch (error) {
     next(error);
   }
